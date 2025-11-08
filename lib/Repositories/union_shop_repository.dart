44b9f95d946ce784d76b4de7fd46fcb92e110972
Dynamite-TH <tedhayward@icloud.com>
2025-11-08 20:51:42 +0000

    Fix product navigation route in UnionShopRepository

--- lib/Repositories/union_shop_repository.dart
+++ lib/Repositories/union_shop_repository.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-class UnionShopRepository {
 
+class UnionShopRepository {
   void navigateToHome(BuildContext context) {
     Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
   }
 
   void navigateToProduct(BuildContext context) {
-    Navigator.pushNamed(context, '/product_page');
+    Navigator.pushNamed(context, '/product');
   }
 
   void navigateToAboutUs(BuildContext context) {
     Navigator.pushNamed(context, '/about_us');
   }
-  
+
   void placeholderCallbackForButtons() {
     // This is the event handler for buttons that don't work yet
   }
