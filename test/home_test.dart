c117815b5905fa275b0603501e436b1cc8ddce40
Dynamite-TH <tedhayward@icloud.com>
2025-11-23 14:58:01 +0000

    Update import statement in home_test.dart to reference home_page.dart

--- test/home_test.dart
+++ test/home_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:union_shop/main.dart';
+import 'package:union_shop/views/home_page.dart';
 
 void main() {
   group('Home Page Tests', () {
