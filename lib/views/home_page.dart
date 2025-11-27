e19531e12d322e72e4d9d482a7b65e5d3ba327cd
Dynamite-TH <tedhayward@icloud.com>
2025-11-27 11:13:53 +0000

    Fix route path for SalesProductScreen in home_page.dart

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -28,7 +28,7 @@ class UnionShopApp extends StatelessWidget {
         '/about_us': (context) => const AboutUsScreen(),
         '/collections': (context) => const CollectionsScreen(),
         '/collections/sales-product': (context) => const SalesProductScreen(),
-        '/collections/sales_product/product': (context) => const ProductPage(),
+        '/collections/sales-product/product': (context) => const ProductPage(),
       },
       // If a route is not found, show the 404 page
       onUnknownRoute: (settings) {
