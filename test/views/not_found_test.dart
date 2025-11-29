03cd6ae8857cb91d05ab10c92b585f7927951af4
Dynamite-TH <tedhayward@icloud.com>
2025-11-29 19:12:54 +0000

    Add widget test for NotFound page to verify 404 message display

---
+++ test/views/not_found_test.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/views/not_found.dart';
+
+import '../test_utils.dart';
+
+void main() {
+  setUp(() {
+    HttpOverrides.global = TestHttpOverrides();
+  });
+
+  testWidgets('Not found page shows 404 message', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: PageNotFoundScreen()));
+    await tester.pumpAndSettle();
+
+    expect(find.textContaining('404 - The page you are looking for does not exist.'), findsOneWidget);
+  });
+}
